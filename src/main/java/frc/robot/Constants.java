f534df1c8878fe8c60fc0d5c1b4f62ed33f15dcb
Nick P <nicolas.a.delapena@gmail.com>
2023-02-06 16:09:18 -0500

    Constants commenting and limelight testing constants
    self explanatory

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,11 +4,22 @@ package frc.robot;
  * This file is used to create an easy to use and edit list of constants in the 
  * robot. Most commonly IDs and ports for electronics.
  */
-public class Constants {    
+public class Constants {
+
+    //motor controller IDs
     public static final int kFrontLeftChannel = 2;
     public static final int kRearLeftChannel = 3;
     public static final int kFrontRightChannel = 1;
     public static final int kRearRightChannel = 0;
 
+    //controller port
     public static final int kXboxPort = 0;
+
+    //target objects dimensions in inches
+    public static final double objSize = 20;
+    //the vertical view angle from forward axis
+    public static final double limelightVerticaViewlAngle = 20.5;
+    //the horizontal view angle from forward axis
+    public static final double limelightHorizontalViewAngle = 27;
+
 }
